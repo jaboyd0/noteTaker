63540ea067f83d287c1764bf5322de7a191e0c80
Joshua Boyd <jaboyd0@gmail.com>
2020-05-02 16:35:37 -0400

    2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var fs = require("fs");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-const mainDir = path.join(__dirname, "/public");
+// const mainDir = path.join(__dirname, "/public");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -22,9 +22,9 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(mainDir, "notes.html"));
-});
+// app.get("/notes", function(req, res) {
+//   res.sendFile(path.join(mainDir, "notes.html"));
+// });
 
 app.get("/api/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "/db/db.json"));
@@ -35,9 +35,9 @@ app.get("/api/notes/:id", function(req, res) {
   res.json(savedNotes[Number(req.params.id)]);
 });
 
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(mainDir, "index.html"));
-// });
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.post("/api/notes", function(req, res) {
   let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
