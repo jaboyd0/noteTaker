4d8fdb506351cba671028fc79dbc2b3074402bb7
Joshua Boyd <jaboyd0@gmail.com>
2020-05-02 16:24:52 -0400

    fixed file paths

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var fs = require("fs");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
+
 const mainDir = path.join(__dirname, "/public");
 
 // Sets up the Express app to handle data parsing
